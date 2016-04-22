b46e79165e1b903ef20d2ca16f5ee7376e421b73
zhangchao <zhangchaoeq@126.com>
2016-04-22 13:30:42 +0800

    init project

---
+++ 1.TapCounter/1.TapCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1.TapCounter
+//
+//  Created by zhangchao on 16/4/22.
+//  Copyright © 2016年 slark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
