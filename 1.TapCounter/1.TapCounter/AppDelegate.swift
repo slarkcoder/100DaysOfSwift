1b522694ed3c79d15dc3d1c8c3ab6e698fe0d500
zhangchao <zhangchaoeq@126.com>
2016-04-22 14:04:52 +0800

    complete first project

--- 1.TapCounter/1.TapCounter/AppDelegate.swift
+++ 1.TapCounter/1.TapCounter/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let viewController = ViewController()
+        let nav = UINavigationController(rootViewController:viewController);
+        self.window?.rootViewController = nav;
+        
         return true
     }
 
--- 1.TapCounter/1.TapCounter/ViewController.swift
+++ 1.TapCounter/1.TapCounter/ViewController.swift
@@ -7,12 +7,61 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var labCount = UILabel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.title = "Tap Counter";
+        
+        let btnRest = UIBarButtonItem(title: "Reset", style: .Plain, target: self, action: #selector(ViewController.resetCount))
+        self.navigationItem.leftBarButtonItem = btnRest
+        
+        labCount = UILabel()
+        labCount.textAlignment = .Center
+        labCount.font = UIFont.systemFontOfSize(100)
+        labCount.textColor = UIColor.blackColor()
+        self.view.addSubview(labCount)
+        
+        let btnAddCount = UIButton()
+        btnAddCount.setTitle("Tap", forState: .Normal)
+        btnAddCount.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        btnAddCount.addTarget(self, action: #selector(ViewController.addCount), forControlEvents: .TouchUpInside)
+        self.view.addSubview(btnAddCount)
+        
+        labCount.snp_makeConstraints { (make) in
+            make.width.equalTo(self.view)
+            make.height.equalTo(100)
+            make.center.equalTo(self.view)
+        }
+        
+        btnAddCount.snp_makeConstraints { (make) in
+            make.width.height.equalTo(60)
+            make.top.equalTo(labCount.snp_bottom)
+            make.centerX.equalTo(self.view)
+        }
+        
+        self.updateCountView()
+    }
+    
+    func updateCountView(){
+        labCount.text = NSString(format: "%d",count) as String
+    }
+    
+    func resetCount(){
+        count = 0
+        self.updateCountView()
+    }
+    
+    func addCount(){
+        count += 1
+        self.updateCountView()
     }
 
     override func didReceiveMemoryWarning() {
